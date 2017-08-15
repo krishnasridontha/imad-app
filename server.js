1067d29f4f0e06f54355b6243231e2e821ec486b
krishnasridontha <31000453+krishnasridontha@users.noreply.github.com>
2017-08-15 19:34:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone ={
+    title: 'Article-one | krishnasri dontha',
+    heading: "Article-one",
+    date: 'aug  14, 2017',
+    star:`
+                                        <p>
+                                             this is my  1 first content article.
+                                          </p>
+                                          <p>
+                                         this is my 2 second content article.
+                                        </p>`
+};
+
+function createTemplate(data) = {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var star = dta.star; 
+    
+var htmltemplate = `
+<html>
+    <head>
+        <title>
+        $ <title>
+        </title>
+               <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+               <div>
+              <a href="/">home</a>
+                    </div>
+                     <hr/>
+                         <h3>
+                         $ {heading}
+                      </h3>
+                          <div>
+                        $ {date}
+                           </div>
+                                  <div>
+                                        $ {star}   
+                                 </div>
+         </div>
+    </body>
+    </head>
+</html>
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-1', function(req,res){
-  res.send('Article two is requested and will be served here');
+  res.send(createTemplate(articleone));
 });
 
 app.get('/article-one', function(req,res){
